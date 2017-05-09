4ed64d3eeb1b6d09d2c74a8ea2abdd8f4bb14fac
bud aminof <budaminof@gmail.com>
2017-05-09 15:41:20 -0600

    refactor: change component to dumb components changed top, middle and footer components. also- added a favicon

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
-class Footer extends Component {
 
-  render() {
-    return (
-      <footer>
-        <p>ALL RIGHTS RESERVED &#9400;</p>
-      </footer>
-    );
-  }
+const Footer = (props) => {
+  return (
+    <footer>
+      <p>ALL RIGHTS RESERVED &#9400;</p>
+    </footer>
+  );
 }
 
 export default Footer;
